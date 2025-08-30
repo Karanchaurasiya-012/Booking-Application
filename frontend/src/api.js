dcf60808f412fddba73cf3d8c8cc3ee2b6234ccb
Karan Chaurasiya <karanchaurasiya.012@gmail.com>
2025-08-30 17:16:05 +0530

    url change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://booking-application-ah21.onrender.com",
+  baseURL: "https://booking-application-ah21.onrender.com/api",
 });
 
 // Add token automatically if present
